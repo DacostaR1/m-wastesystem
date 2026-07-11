55c79f24af09e8a0ba35493e3a68772c3af56673
Ronald <afekurobert107@gmail.com>
2026-07-11 20:02:18 +0300

    Continuous deployment /Pipeline (Emailnotifications Integrations)

--- email.js
+++ email.js
@@ -1,6 +1,5 @@
 const nodemailer = require("nodemailer");
 
-
 const transporter = nodemailer.createTransport({
 
     host: process.env.EMAIL_HOST,
@@ -10,11 +9,8 @@ const transporter = nodemailer.createTransport({
     secure: false,
 
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASS
-
     }
 
 });
